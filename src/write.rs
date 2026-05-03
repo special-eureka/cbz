addba611b5c9721d9c3bf072b978b89ef6f71b99
tonymushah <tonyramanantsalama@gmail.com>
2026-05-03 09:27:26 +0300

    Update write.rs

--- src/write.rs
+++ src/write.rs
@@ -4,8 +4,8 @@ use std::{io::Read, path::Path};
 
 pub trait ComicBookWriter {
     type Error;
-    fn add_image(&self, image: image::DynamicImage) -> Result<(), Self::Error>;
-    fn add_file<P, R>(&self, path: P, file: R) -> Result<(), Self::Error>
+    fn add_image(&mut self, image: image::DynamicImage) -> Result<(), Self::Error>;
+    fn add_file<P, R>(&mut self, path: P, file: R) -> Result<(), Self::Error>
     where
         P: AsRef<Path>,
         R: Read;
