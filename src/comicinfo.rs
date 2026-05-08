e1bd191b68f5426ae12433b942cb1482c345f9c7
tonymushah <tonyramanantsalama@gmail.com>
2026-05-08 03:13:32 +0300

    Update comicinfo.rs

--- src/comicinfo.rs
+++ src/comicinfo.rs
@@ -7,4 +7,5 @@
 //!
 //! Only [`v2`](https://github.com/anansi-project/comicinfo/blob/db8e1d84132f97403b226f2e12aaec1342c2a223/schema/v2.0/ComicInfo.xsd) is supported.
 //!
+pub mod manga;
 pub mod yes_no;
