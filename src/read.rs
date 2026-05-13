a051186573806dd9997accb8fe282637c6769995
tonymushah <tonyramanantsalama@gmail.com>
2026-05-13 22:36:00 +0300

    typo changes

--- src/read.rs
+++ src/read.rs
@@ -10,46 +10,46 @@ pub mod comicinfo;
 pub trait ComicBookReader {
     type Error;
     /// Get the list of images path as it is ordered from the archive
-    fn images_unordered(&self) -> Vec<String>;
+    fn pages_unordered(&self) -> Vec<String>;
     /// Get list of images paths
     ///
     /// This should return a [`Vec<String>`] of alphabetically (case insensitive) ordered of images path
-    fn images(&self) -> Vec<String> {
-        let mut images = self.images_unordered();
+    fn pages(&self) -> Vec<String> {
+        let mut images = self.pages_unordered();
         images.sort_by_key(|s| s.to_lowercase());
         images
     }
     /// Get a file from the archive
     fn get_file(&mut self, file: &str) -> Result<Vec<u8>, Self::Error>;
     /// Get image by its path
-    fn get_image_by_path(&mut self, image: &str) -> Result<Vec<u8>, Self::Error> {
+    fn get_page_by_path(&mut self, image: &str) -> Result<Vec<u8>, Self::Error> {
         self.get_file(image)
     }
     /// Get image by its index
     ///
-    /// This one uses [`Self::images`] by default.
+    /// This one uses [`Self::pages`] by default.
     ///
-    /// Use [`Self::get_image_by_index_unordered`] if you want to use the [`Self::images_unordered`]
+    /// Use [`Self::get_page_by_index_unordered`] if you want to use the [`Self::pages_unordered`]
     /// NOTE: The default implementation calls [`Self::images`] and uses [`[T]::get`] to get the image path.
-    fn get_image_by_index(&mut self, index: usize) -> Result<Option<Vec<u8>>, Self::Error> {
-        let images = self.images();
+    fn get_page_by_index(&mut self, index: usize) -> Result<Option<Vec<u8>>, Self::Error> {
+        let images = self.pages();
         let Some(image_path) = images.get(index) else {
             return Ok(None);
         };
-        Ok(Some(self.get_image_by_path(image_path)?))
+        Ok(Some(self.get_page_by_path(image_path)?))
     }
 
     /// Get image by its index
     ///
-    /// This one uses [`Self::images_unordered`] by default.
-    fn get_image_by_index_unordered(
+    /// This one uses [`Self::pages_unordered`] by default.
+    fn get_page_by_index_unordered(
         &mut self,
         index: usize,
     ) -> Result<Option<Vec<u8>>, Self::Error> {
-        let images = self.images_unordered();
+        let images = self.pages_unordered();
         let Some(image_path) = images.get(index) else {
             return Ok(None);
         };
-        Ok(Some(self.get_image_by_path(image_path)?))
+        Ok(Some(self.get_page_by_path(image_path)?))
     }
 }
--- src/write.rs
+++ src/write.rs
@@ -7,21 +7,40 @@ use std::{io::Read, path::Path};
 pub trait ComicBookWriter {
     type Error;
     /// Add a page at the Comic book archive
-    fn add_image(&mut self, image: image::DynamicImage) -> Result<(), Self::Error>;
+    ///
+    /// If the `comicinfo` feature flag is enabled,
+    /// all writers should normally provide the [`Image`](crate::comicinfo::comic_page_info::image),
+    /// [`ImageWidth`](crate::comicinfo::comic_page_info::image_width),
+    /// [`ImageHeight`](crate::comicinfo::comic_page_info::image_height) attributes on `ComicInfo` `Page` elements.
+    fn add_page(&mut self, image: image::DynamicImage) -> Result<(), Self::Error>;
     /// Similar to the [`Self::add_image`]
     /// but take additional [`image::ImageFormat`] as an argument which is the initial format the image. (Since we can't extract format via [`image::DynamicImage`]).
     ///
     /// The default implementation just call [`Self::add_image`] but there are some edge cases where you might need this. (Like handling GIF images for example)
-    fn add_image_with_format(
+    fn add_page_with_format(
         &mut self,
         image: image::DynamicImage,
         _format: image::ImageFormat,
     ) -> Result<(), Self::Error> {
-        self.add_image(image)
+        self.add_page(image)
     }
     /// Add an additional file to the archive
     fn add_file<P, R>(&mut self, path: P, file: R) -> Result<(), Self::Error>
     where
         P: AsRef<Path>,
         R: Read;
+
+    /// Similiar to [`Self::add_image_with_format`] but will allow you add
+    ///
+    #[cfg(feature = "comicinfo")]
+    #[cfg_attr(docsrs, doc(feature = "comicinfo"))]
+    fn add_page_with_page_type(
+        &mut self,
+        image: image::DynamicImage,
+        _format: image::ImageFormat,
+        _page_type: crate::comicinfo::comic_page_type::ComicPageType,
+        _doublepage: bool,
+    ) -> Result<(), Self::Error> {
+        self.add_page(image)
+    }
 }
